a2e729fba2a1fa82eaf274a7a17435124c0b08be
printfn <printfn@users.noreply.github.com>
2022-11-13 00:14:12 +1300

    Use a multi-line string

--- quine-putchar.c
+++ quine-putchar.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 
-void printer(const char *s, int r) {
-    const char *s2 = s;
-    char c;
+static char *S = "#include <stdio.h>~\
+~\
+static char *S = `;~\
+~\
+void printer(int r) {~\
+    char c, *s = S;~\
+    while ((c = *s++)) {~\
+        if (c == 126 && r) putchar(10);~\
+        else if (c == 126) { putchar(126); putchar(92); putchar(10); }~\
+        else if (c == 96 && r) { putchar(34); printer(0); putchar(34); }~\
+        else putchar(c);~\
+    }~\
+}~\
+~\
+int main(void) {~\
+    printer(1);~\
+}~\
+";
+
+void printer(int r) {
+    char c, *s = S;
     while ((c = *s++)) {
         if (c == 126 && r) putchar(10);
-        else if (c == 96 && r) {
-            putchar(34);
-            printer(s2, 0);
-            putchar(34);
-        }
+        else if (c == 126) { putchar(126); putchar(92); putchar(10); }
+        else if (c == 96 && r) { putchar(34); printer(0); putchar(34); }
         else putchar(c);
     }
 }
 
 int main(void) {
-    const char *s = "#include <stdio.h>~~void printer(const char *s, int r) {~    const char *s2 = s;~    char c;~    while ((c = *s++)) {~        if (c == 126 && r) putchar(10);~        else if (c == 96 && r) {~            putchar(34);~            printer(s2, 0);~            putchar(34);~        }~        else putchar(c);~    }~}~~int main(void) {~    const char *s = `;~    printer(s, 1);~}~";
-    printer(s, 1);
+    printer(1);
 }
